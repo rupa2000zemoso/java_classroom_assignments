2413dfe98850cac05a6342af1a2443762b615f10
Rupa Reddy <rupa.vudumula@zemosolabs.com>
2022-04-20 18:34:44 +0530

    assignment 9

---
+++ src/Assignment9/RegularExpression.java
@@ -0,0 +1,15 @@
+package Assignment9;
+
+import java.util.regex.Pattern;
+
+public class RegularExpression
+{
+        public static void main(String[] args) {
+            String str1="Am learning regex.";
+            String str2="its fun learning this.";
+            String str3="Hello";
+
+            String regexpvar = "[A-Z].*\\.";
+            System.out.println(Pattern.matches(regexpvar,str3));
+        }
+}
